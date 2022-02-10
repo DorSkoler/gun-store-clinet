700e074bf6d72be1fd55d1c46807211d5d7136a7
Dor Skoler <dorskoler@gmail.com>
2022-02-10 18:25:55 +0200

    images path

--- src/weapons/weaponsHardCoded.js
+++ src/weapons/weaponsHardCoded.js
@@ -5,28 +5,28 @@ export default [
     name: "Combat Knife",
     price: "0.0001",
     type: "Cold",
-    img: "../../public/images/Guns/Cold/Combat-Knife.png",
+    img: "/images/Guns/Cold/Combat-Knife.png",
   },
   {
     id: 2,
     name: "Dagger",
     price: "0.0002",
     type: "Cold",
-    img: '../../public/images/Guns/Cold/Dagger.png',
+    img: '/images/Guns/Cold/Dagger.png',
   },
   {
     id: 3,
     name: "Pocket Knife",
     price: "0.0003",
     type: "Cold",
-    img: "../../public/images/Guns/Cold/Pocket-Knife.png",
+    img: "/images/Guns/Cold/Pocket-Knife.png",
   },
   {
     id: 4,
     name: "Warrior Knife",
     price: "0.0004",
     type: "Cold",
-    img: "../../public/images/Guns/Cold/Warrior-Knife.png",
+    img: "/images/Guns/Cold/Warrior-Knife.png",
   },
   //------Pistol-----//
   {
@@ -34,84 +34,84 @@ export default [
     name: "Beretta",
     price: "0.004",
     type: "Pistol",
-    img: "../../public/images/Guns/Pistol/Beretta.png",
+    img: "/images/Guns/Pistol/Beretta.png",
   },
   {
     id: 6,
     name: "CZ-75",
     price: "0.007",
     type: "Pistol",
-    img: "../../public/images/Guns/Pistol/CZ75.png",
+    img: "/images/Guns/Pistol/CZ75.png",
   },
   {
     id: 7,
     name: "Desert Eagle",
     price: "0.005",
     type: "Pistol",
-    img: "../../public/images/Guns/Pistol/Desert-Eagle.png",
+    img: "/images/Guns/Pistol/Desert-Eagle.png",
   },
   {
     id: 8,
     name: "FN-5",
     price: "0.002",
     type: "Pistol",
-    img: "../../public/images/Guns/Pistol/FN5.png",
+    img: "/images/Guns/Pistol/FN5.png",
   },
   {
     id: 9,
     name: "Glock",
     price: "0.003",
     type: "Pistol",
-    img: "../../public/images/Guns/Pistol/Glock.png",
+    img: "/images/Guns/Pistol/Glock.png",
   },
   {
     id: 10,
     name: "HK-45",
     price: "0.0044",
     type: "Pistol",
-    img: "../../public/images/Guns/Pistol/HK45.png",
+    img: "/images/Guns/Pistol/HK45.png",
   },
   {
     id: 11,
     name: "Jericho-941",
     price: "0.0095",
     type: "Pistol",
-    img: "../../public/images/Guns/Pistol/Jericho-941.png",
+    img: "/images/Guns/Pistol/Jericho-941.png",
   },
   {
     id: 12,
     name: "K-100",
     price: "0.0067",
     type: "Pistol",
-    img: "../../public/images/Guns/Pistol/K100.png",
+    img: "/images/Guns/Pistol/K100.png",
   },
   {
     id: 13,
     name: "Makarov",
     price: "0.0012",
     type: "Pistol",
-    img: "../../public/images/Guns/Pistol/Makarov.png",
+    img: "/images/Guns/Pistol/Makarov.png",
   },
   {
     id: 14,
     name: "P-227",
     price: "0.0032",
     type: "Pistol",
-    img: "../../public/images/Guns/Pistol/P227.png",
+    img: "/images/Guns/Pistol/P227.png",
   },
   {
     id: 15,
     name: "Remington-R1",
     price: "0.0046",
     type: "Pistol",
-    img: "../../public/images/Guns/Pistol/Remington-R1.png",
+    img: "/images/Guns/Pistol/Remington-R1.png",
   },
   {
     id: 16,
     name: "SW-1911",
     price: "0.0073",
     type: "Pistol",
-    img: "../../public/images/Guns/Pistol/SW1911.png",
+    img: "/images/Guns/Pistol/SW1911.png",
   },
   //----Shotguns-----//
   {
@@ -119,28 +119,28 @@ export default [
     name: "Benelli - M1014",
     price: "0.013",
     type: "Shotgun",
-    img: "../../public/images/Guns/Shotgun/Benelli-M1014.png",
+    img: "/images/Guns/Shotgun/Benelli-M1014.png",
   },
   {
     id: 18,
     name: "KelTec KSG",
     price: "0.016",
     type: "Shotgun",
-    img: "../../public/images/Guns/Shotgun/Kel-Tec-KSG.png",
+    img: "/images/Guns/Shotgun/Kel-Tec-KSG.png",
   },
   {
     id: 19,
     name: "SPAS-12",
     price: "0.0178",
     type: "Shotgun",
-    img: "../../public/images/Guns/Shotgun/SPAS-12.png",
+    img: "/images/Guns/Shotgun/SPAS-12.png",
   },
   {
     id: 20,
     name: "Winchester-1200",
     price: "0.014",
     type: "Shotgun",
-    img: "../../public/images/Guns/Shotgun/Winchester-Model-1200.png",
+    img: "/images/Guns/Shotgun/Winchester-Model-1200.png",
   },
   //---Light---//
   {
@@ -148,28 +148,28 @@ export default [
     name: "CZ-Scorpion",
     price: "0.016",
     type: "Light",
-    img: "../../public/images/Guns/Light/CZ-Scorpion.png",
+    img: "/images/Guns/Light/CZ-Scorpion.png",
   },
   {
     id: 22,
     name: "Kriss Vector",
     price: "0.025",
     type: "Light",
-    img: "../../public/images/Guns/Light/KRISS-Vector.png",
+    img: "/images/Guns/Light/KRISS-Vector.png",
   },
   {
     id: 23,
     name: "MP5",
     price: "0.0169",
     type: "Light",
-    img: "../../public/images/Guns/Light/MP5.png",
+    img: "/images/Guns/Light/MP5.png",
   },
   {
     id: 24,
     name:"MP7",
     price:"0.013",
     type:"Light",
-    img:"../../public/images/Guns/Light/MP7.png",
+    img:"/images/Guns/Light/MP7.png",
    
    },
    {
@@ -177,7 +177,7 @@ export default [
     name:"P90",
     price:"0.034",
     type:"Light",
-    img:"../../public/images/Guns/Light/P90.png",
+    img:"/images/Guns/Light/P90.png",
    
    },
    {
@@ -185,7 +185,7 @@ export default [
     name:"PPSH-41",
     price:"0.018",
     type:"Light",
-    img:"../../public/images/Guns/Light/PPSH-41.png",
+    img:"/images/Guns/Light/PPSH-41.png",
    
    },
    {
@@ -193,7 +193,7 @@ export default [
     name:"Thompson",
     price:"0.032",
     type:"Light",
-    img:"../../public/images/Guns/Light/Thompson.png",
+    img:"/images/Guns/Light/Thompson.png",
    
    },
    {
@@ -201,7 +201,7 @@ export default [
     name:"Uzi",
     price:"0.04",
     type:"Light",
-    img:"../../public/images/Guns/Light/UZI.png",
+    img:"/images/Guns/Light/UZI.png",
    
    },
    //------Heavy------//
@@ -210,7 +210,7 @@ export default [
     name:"AK-12",
     price:"0.065",
     type:"Heavy",
-    img:"../../public/images/Guns/Heavy/AK12.png",
+    img:"/images/Guns/Heavy/AK12.png",
    
    },
    {
@@ -218,7 +218,7 @@ export default [
     name:"AK-47",
     price:"0.075",
     type:"Heavy",
-    img:"../../public/images/Guns/Heavy/AK47.png",
+    img:"/images/Guns/Heavy/AK47.png",
    
    },
    {
@@ -226,7 +226,7 @@ export default [
     name:"AN-94",
     price:"0.032",
     type:"Heavy",
-    img:"../../public/images/Guns/Heavy/AN-94.png",
+    img:"/images/Guns/Heavy/AN-94.png",
    
    },
    {
@@ -234,7 +234,7 @@ export default [
     name:"AUG A1",
     price:"0.045",
     type:"Heavy",
-    img:"../../public/images/Guns/Heavy/AUG_A1.png",
+    img:"/images/Guns/Heavy/AUG_A1.png",
    
    },
    {
@@ -242,7 +242,7 @@ export default [
     name:"FN-F2000",
     price:"0.065",
     type:"Heavy",
-    img:"../../public/images/Guns/Heavy/FN-F2000.png",
+    img:"/images/Guns/Heavy/FN-F2000.png",
    
    },
    {
@@ -250,7 +250,7 @@ export default [
     name:"FN-FAL",
     price:"0.025",
     type:"Heavy",
-    img:"../../public/images/Guns/Heavy/FN-FAL.png",
+    img:"/images/Guns/Heavy/FN-FAL.png",
    
    },
    {
@@ -258,7 +258,7 @@ export default [
     name:"FN-SCAR",
     price:"0.015",
     type:"Heavy",
-    img:"../../public/images/Guns/Heavy/FN-SCAR.png",
+    img:"/images/Guns/Heavy/FN-SCAR.png",
    
    },
    {
@@ -266,7 +266,7 @@ export default [
     name:"Galil",
     price:"0.043",
     type:"Heavy",
-    img:"../../public/images/Guns/Heavy/Galil.png",
+    img:"/images/Guns/Heavy/Galil.png",
    
    },
    {
@@ -274,7 +274,7 @@ export default [
     name:"HK-G36",
     price:"0.019",
     type:"Heavy",
-    img:"../../public/images/Guns/Heavy/HK-G36.png",
+    img:"/images/Guns/Heavy/HK-G36.png",
    
    },
    {
@@ -282,7 +282,7 @@ export default [
     name:"KelTec RFB",
     price:"0.037",
     type:"Heavy",
-    img:"../../public/images/Guns/Heavy/Kel-Tec-RFB.png",
+    img:"/images/Guns/Heavy/Kel-Tec-RFB.png",
    
    },
    {
@@ -290,7 +290,7 @@ export default [
     name:"L85",
     price:"0.085",
     type:"Heavy",
-    img:"../../public/images/Guns/Heavy/L85.png",
+    img:"/images/Guns/Heavy/L85.png",
    
    },
    {
@@ -298,7 +298,7 @@ export default [
     name:"Carbine M1",
     price:"0.093",
     type:"Heavy",
-    img:"../../public/images/Guns/Heavy/M1-Carbine.png",
+    img:"/images/Guns/Heavy/M1-Carbine.png",
    
    },
    {
@@ -306,7 +306,7 @@ export default [
     name:"M4A1",
     price:"0.041",
     type:"Heavy",
-    img:"../../public/images/Guns/Heavy/M4A1.png",
+    img:"/images/Guns/Heavy/M4A1.png",
    
    },
    {
@@ -314,7 +314,7 @@ export default [
     name:"M16",
     price:"0.016",
     type:"Heavy",
-    img:"../../public/images/Guns/Heavy/M16.png",
+    img:"/images/Guns/Heavy/M16.png",
    
    },
    {
@@ -322,7 +322,7 @@ export default [
     name:"M249",
     price:"0.0249",
     type:"Heavy",
-    img:"../../public/images/Guns/Heavy/M249.png",
+    img:"/images/Guns/Heavy/M249.png",
    
    },
    {
@@ -330,7 +330,7 @@ export default [
     name:"STG-44",
     price:"0.044",
     type:"Heavy",
-    img:"../../public/images/Guns/Heavy/STG44.png",
+    img:"/images/Guns/Heavy/STG44.png",
    
    },
    {
@@ -338,7 +338,7 @@ export default [
     name:"X95",
     price:"0.095",
     type:"Heavy",
-    img:"../../public/images/Guns/Heavy/X95.png",
+    img:"/images/Guns/Heavy/X95.png",
    
    },
    {
@@ -346,7 +346,7 @@ export default [
     name:"XM8",
     price:"0.08",
     type:"Heavy",
-    img:"../../public/images/Guns/Heavy/XM8.png",
+    img:"/images/Guns/Heavy/XM8.png",
    },
    //-----Sniper-----/
    {
@@ -354,42 +354,42 @@ export default [
     name:"AWP",
     price:"0.09",
     type:"Sniper",
-    img:"../../public/images/Guns/Sniper/AWP.png",
+    img:"/images/Guns/Sniper/AWP.png",
    },
    {
     id: 48,
     name:"Dragunov SVD",
     price:"0.032",
     type:"Sniper",
-    img:"../../public/images/Guns/Sniper/Dragunov-SVD.png",
+    img:"/images/Guns/Sniper/Dragunov-SVD.png",
    },
    {
     id: 49,
     name:"DSR-1",
     price:"0.045",
     type:"Sniper",
-    img:"../../public/images/Guns/Sniper/DSR-1.png",
+    img:"/images/Guns/Sniper/DSR-1.png",
    },
    {
     id: 50,
     name:"M24",
     price:"0.026",
     type:"Sniper",
-    img:"../../public/images/Guns/Sniper/M24.png",
+    img:"/images/Guns/Sniper/M24.png",
    },
    {
     id: 51,
     name:"SV-98",
     price:"0.038",
     type:"Sniper",
-    img:"../../public/images/Guns/Sniper/SV-98.png",
+    img:"/images/Guns/Sniper/SV-98.png",
    },
    {
     id: 52,
     name:"M95",
     price:"0.1",
     type:"Sniper",
-    img:"../../public/images/Guns/Sniper/M95.png",
+    img:"/images/Guns/Sniper/M95.png",
    }
 
 ];
