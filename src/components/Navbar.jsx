f9700fb72c78f3171376592828baed4c1d6ddb8c
Dor Skoler <dorskoler@gmail.com>
2022-02-10 18:16:22 +0200

    images path

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React, {useContext} from 'react';
-import logo from '/images/logo.png';
+import logo from '../../public/images/logo.png';
 // import gun from '../images/Guns/Light/CZ-Scorpion-removebg-preview.png'
 import {TransactionContext} from "../context/TransactionContext"
 import { Link } from 'react-router-dom';
