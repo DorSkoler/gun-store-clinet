9f6e73dff9d09b00c1eaaf22b45ca9e9e1122722
Dor Skoler <dorskoler@gmail.com>
2022-03-05 11:03:46 +0200

    new contract

--- src/utils_contract/details.js
+++ src/utils_contract/details.js
@@ -1,5 +1,5 @@
 import abi from './TsxABI.json'
 
 export const contractABI = abi.abi;
-export const contractAddressABI = '0xb007394a7EA909047f2381B323F78f7f83794AB3'
+export const contractAddressABI = '0x4F7aD3Ae8D01E1E72D76A2Ad60C6BDa798F504CA'
 export const gunStoreAddress = '0x4aF7c85FC637aFD8E6e17903e165667343136ce7'
